b9f028e1c968b7c46c59cfea6f1ee4201200ceee
Genmurraya <156489097+Genmurraya@users.noreply.github.com>
2025-06-30 09:14:35 +0800

    Aplikasi sudah berjalan di local (#1)
    * Aplikasi sudah berjalan di local
    
    * merging new process
    
    ---------
    
    Co-authored-by: Agresia Rettha <54567254+Agresiarthtp@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,120 @@
+import java.util.Scanner;
+
+class Node {
+    String name;
+    Node next;
+
+    public Node(String name) {
+        this.name = name;
+        this.next = null;
+    }
+}
+
+class CustomerQueue {
+    private Node head, tail;
+
+    // menambahkan ke belakang antrian
+    public void push(String name){
+        Node newNode = new Node(name);
+        if (head == null){
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        System.out.println( " " + name);
+    }
+
+    // melayani pelanggan dari antrian yang didepan
+    public String pop(){
+        if (head == null){
+            return null;
+        } else {
+            String name = head.name;
+            head = head.next;
+            return name;
+        }
+    }
+
+    // menampilkan seluruh antrian
+    public String showQueueList(){
+       if (head == null){
+           System.out.println("Queue is empty");
+       } else {
+           System.out.println("\nCurrent Queue List :");
+           Node current = head;
+           while (current != null){
+               System.out.println(" " + current.name);
+               current = current.next;
+           }
+       }
+       return null;
+
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println( "Team Assignment 2");
+        Scanner scanner = new Scanner(System.in);
+       CustomerQueue queue = new CustomerQueue();
+
+        System.out.println("--Aplikasi Antrian Restoran--");
+
+        while(true){
+            System.out.println("\nMenu:");
+            System.out.println("1. Tambahkan Antrian Pelanggan:");
+            System.out.println("2. Layani Antrian");
+            System.out.println("3. Lihat Antrian");
+            System.out.println("4. Keluar");
+            System.out.println("Pilih opsi: ");
+
+            int pilihan = -1;
+            try {
+                pilihan =scanner.nextInt();
+            }catch(Exception e){
+                System.out.println("Input tidak valid! Harap masukkan angka.");
+                scanner.next(); // Membersihkan buffer scanner
+                continue;
+            }
+            scanner.nextLine(); // Membersihkan newline character dari buffer
+
+            switch(pilihan){
+                case 1:
+                    System.out.println("Masukkan nama pelanggan: ");
+                    String data = scanner.nextLine();
+                    queue.push(data);
+                    queue.showQueueList();
+                    break;
+
+                case 2:
+                    queue.pop();
+                    System.out.println("Antrian telah dilayani\n");
+                    queue.showQueueList();
+                    break;
+
+                case 3:
+                    queue.showQueueList();
+                    break;
+
+                case 4:
+                    System.out.println("Keluar dari sistem");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Pilih tidak valid, silahkan pilih angka 1-4. ");
+            }
+        }
+
+//       queue.push("Agresia"); // masuk pada barisan pertama
+//       queue.push("Putu"); // masuk pada barisan kedua
+//       queue.push("Tria"); // masuk pada barisan ketiga
+//       queue.push("Fadli"); // masuk pada barisan keempat
+//       queue.push("Luthfi"); // masuk pada barisan kelima
+//
+//       queue.pop(); // ketika barisan pertama dilayani
+//       System.out.println(queue.showQueueList());
+//       queue.pop(); // ketika barisan kedua dilayani
+//       System.out.println(queue.showQueueList());
     }
 }
